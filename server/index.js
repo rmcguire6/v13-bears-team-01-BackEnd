001d6cea250c81d55e1fc54c34294ae2f6a3b9a9
Regina Mc Guire <rmcguire@antelecom.net>
2019-11-22 16:47:27 -0800

    Add sqlite database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 
 const app = express()
-
+require('./db/db')
 app.get('/', (req, res) => {
   res.send('Welcome to Location Reminders! Please login or signup')
 })
 
-const PORT = 5000
-app.listen(PORT)
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`)
+})
