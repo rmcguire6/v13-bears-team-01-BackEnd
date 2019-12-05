547688cbedf594c73f04f7bd4e1fef3937f41a2b
Regina Mc Guire <rmcguire@antelecom.net>
2019-12-05 12:49:23 -0800

    Add sqlite migration

---
+++ server/migrations/20191205182903-create-reminder.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Reminders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      text: {
+        type: Sequelize.STRING
+      },
+      list: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATEONLY
+      },
+      time: {
+        type: Sequelize.DATE
+      },
+      location: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Reminders');
+  }
+};
