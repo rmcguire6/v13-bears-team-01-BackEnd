45cf1a52192768cafdad2eecccc72c73e0500d32
Regina Mc Guire <rmcguire@antelecom.net>
2019-12-19 17:48:50 -0800

    Add navigation to Reminder Item

--- server/client/src/components/reminder-item/ReminderItem.js
+++ server/client/src/components/reminder-item/ReminderItem.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { List } from 'semantic-ui-react'
 
-const ReminderItem = (props) => {
+const ReminderItem = ({ id, text, location, date, list }) => {
+  const history = useHistory()
   return (
     <section>
-      <List.Item><List.Icon name='square' />
-        {props.text ? props.text : ''}
-        {props.location ? <List.Icon name='location arrow' /> : ''}
-        {props.date ? props.reminder.date : ''}
-        {props.list ? props.reminder.list : ''}
+      <List.Item>
+        <Link to={`/edit/${id}`}>
+          <List.Icon name='square' />
+        </Link>
+        {text}
+        {location ? <List.Icon name='location arrow' /> : ''}
+        {date ? date.slice(0, 21).toString() : ''}
+        <List.Icon name='chevron right' onClick={() => { history.push(`/reminder/${id}`) }} />
       </List.Item>
+
     </section>
   )
 }
