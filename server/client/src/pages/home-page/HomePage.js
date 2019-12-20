e4da0d001c9aca662f8aab26f1f3fd89fd2e0e61
Regina Mc Guire <rmcguire@antelecom.net>
2019-12-19 17:13:34 -0800

    Add navigation to HomePage

--- server/client/src/pages/home-page/HomePage.js
+++ server/client/src/pages/home-page/HomePage.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { Button, Container, Header, List } from 'semantic-ui-react'
 import ReminderList from '../../components/reminder-list/ReminderList'
 import ListList from '../../components/list-list/ListList'
 import './home-page.styles.css'
 
 const HomePage = (props) => {
+  const history = useHistory()
   return (
     <div id='home-page'>
       <Container text>
         <section id='reminders'>
           <Header as='h2'>Reminders</Header>
           <List celled>
-            <ReminderList reminders={props.reminders} />
+            {props.reminders ? <ReminderList reminders={props.reminders} /> : ''}
           </List>
         </section>
         <section id='lists'>
@@ -20,7 +22,7 @@ const HomePage = (props) => {
             <ListList reminders={props.reminders} />
           </List>
         </section>
-        <Button circular icon='plus' floated='right' color='green' />
+        <Button onClick={() => { history.push('/create') }} circular icon='plus' floated='right' color='green' />
       </Container>
     </div>
   )
